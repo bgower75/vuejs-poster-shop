d4fcf780dfc693a5f4da0f1f5439295d31c3c75c
Belinda Gower <belinda.gower@simplyhealth.co.uk>
2018-08-28 17:19:43 +0100

    added qty buttons, formatted prices

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+var PRICE = 9.99;
 new Vue ({
   el: '#app',
   data: {
@@ -11,22 +12,43 @@ new Vue ({
   },
   methods: {
     addItem: function (index) {
-      this.total += 9.99;
+      this.total += PRICE;
       var item = this.items[index];
       var found = false;
-      for (var i = 0; i < this.cart.length; i++) {
-        if (this.cart[i].id === item.id) {
+      for (var i = 0; i < this.cart.length; i++) { //loop through the cart items
+        if (this.cart[i].id === item.id) { //if item is found in the cart show the quantity
           found = true;
           this.cart[i].qty++;
         }
       }
-      if (!found) {
+      if (!found) { //if item not in the cart put it in and show quantity and price
         this.cart.push({
           id: item.id,
           title: item.title,
-          qty: 1
+          qty: 1,
+          price: PRICE
         })
       }
+    },
+    increase: function (item) {
+      item.qty++; //increment the item quantity by 1
+      this.total += PRICE; //increment the price by 1
+    },
+    decrease: function (item) {
+      item.qty--; //decrement the quantity by 1
+      this.total -= PRICE; //decrement the price by 1
+      if (item.qty <= 0) { //if the item qty is set to 0
+        for (var i=0; i<this.cart.length; i++) //loop through cart and
+          if (this.cart[i].id === item.id) {//if the item is the one set to 0
+            this.cart.splice(i,1); //remove it from the cart *splice removes using the index of the item and how many
+            break; //exit the for loop
+          }
+      }
+    }
+  },
+  filters: {
+    currency: function(price) {
+      return '£'.concat(price.toFixed(2)); //set the price to have a £ infront and also convert to a 2 decimal string value
     }
   }
 });
