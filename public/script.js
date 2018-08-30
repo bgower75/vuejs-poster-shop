859914a8d371db3e04fbf295375e7925266cae47
Belinda Gower <belinda.gower@simplyhealth.co.uk>
2018-08-30 17:36:02 +0100

    finished product search

--- public/script.js
+++ public/script.js
@@ -3,12 +3,11 @@ new Vue ({
   el: '#app',
   data: {
     total: 0,
-    items: [
-      { id: 1, title: 'Item1'},
-      { id: 2, title: 'Item2'},
-      { id: 3, title: 'Item3'}
-    ],
-    cart: []
+    items: [],
+    cart: [],
+    search: '',
+    lastSearch: '',
+    loading: false
   },
   methods: {
     addItem: function (index) {
@@ -39,18 +38,27 @@ new Vue ({
       item.qty--; //decrement the quantity by 1
       this.total -= PRICE; //decrement the price by 1
       if (item.qty <= 0) { //if the item qty is set to 0
-        for (var i=0; i<this.cart.length; i++) //loop through cart and
+        for (var i=0; i<this.cart.length; i++) {//loop through cart and
           if (this.cart[i].id === item.id) {//if the item is the one set to 0
             this.cart.splice(i,1); //remove it from the cart *splice removes using the index of the item and how many
             break; //exit the for loop
           }
+        }
       }
+    },
+    onSubmit: function () {
+      this.items = [];
+      this.loading = true;
+      this.$http.get('/search/'.concat(this.search)).then(function (res) { //getting results from imgur server using /search/:query returning results that relate to what was input in the search box
+        this.lastSearch = this.search; //set the lastsearch to what was last entered in the input field
+        this.items = res.data; //data received now put into items
+        this.loading = false;
+      });
     }
   },
   filters: {
-    currency: function(price) {
+    currency: function (price) {
       return '£'.concat(price.toFixed(2)); //set the price to have a £ infront and also convert to a 2 decimal string value
     }
   }
 });
-
